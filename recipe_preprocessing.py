bb47de633c806f078c75233f19e72a0e2f0002fd
meanseo <chlalstjok97@gmail.com>
2022-10-30 00:55:17 +0900

    데이터 정리

--- recipe_preprocessing.py
+++ recipe_preprocessing.py
@@ -1,7 +1,24 @@
+from importlib.resources import contents
+import re
 import pandas as pd
 
 data = pd.read_csv('data/recipe_data.csv', encoding="cp949")
 data.rename(columns=data.iloc[0], inplace=True) # 0번째 열을 칼럼명으로 지정
 data.drop(0, axis=0, inplace=True) #0번째 열 삭제
 data = data[['레시피일련번호','요리명','스크랩수','요리방법별명','요리내용']] # 특정 칼럼 추출
-print(data.head(5))
+print(data['요리내용'])
+# data.info()
+'''
+ 0   레시피일련번호  128400 non-null  object
+ 1   요리명      126310 non-null  object
+ 2   스크랩수     128400 non-null  object
+ 3   요리방법별명   128400 non-null  object
+ 4   요리내용     127340 non-null  object
+'''
+
+# for i in range(1, len(data.index)):
+#     word = ['[재료]', '[필수재료]', '[지급재료]']
+#     content = data.loc[i,'요리내용']
+#     # res = re.sub('[재료]','', content)
+# print(content)
+    
